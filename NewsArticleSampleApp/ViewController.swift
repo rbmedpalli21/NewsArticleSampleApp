011a05d6bdebd2e5378b0daf33a243c4a13543cc
Rakesh Medpalli <48820419+rbmedpalli21@users.noreply.github.com>
2022-07-01 12:13:29 +0530

    Initial Commit

---
+++ NewsArticleSampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsArticleSampleApp
+//
+//  Created by Rakesh Medpalli on 01/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
